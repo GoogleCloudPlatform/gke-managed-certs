9c041f859904285f17706dde1dd2b85a6b5ea21a
Krzysztof Kwaśniewski <krzyk@google.com>
2018-07-17 18:49:09 +0200

    set namespace for kubectl

--- hack/e2e.sh
+++ hack/e2e.sh
@@ -6,7 +6,11 @@ set -o pipefail
 
 SCRIPT_ROOT=$(dirname ${BASH_SOURCE})/..
 
-echo -ne "Deploy components for e2e tests"
+echo -ne "Define context for kubectl\n"
+kubectl config set-context continuous_integration --namespace="continuous_integration"
+kubectl config use-context continuous_integration
+
+echo -ne "Deploy components for e2e tests\n"
 ${SCRIPT_ROOT}/hack/deploy-for-e2e.sh
 
 echo -ne `(kubectl get pods -o yaml)`
