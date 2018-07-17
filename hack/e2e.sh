0ccf839ba20f0bd8e2b4f4c6cfd65036a9523e0a
Krzysztof Kwaśniewski <krzyk@google.com>
2018-07-17 16:35:46 +0200

    corrected path

--- hack/e2e.sh
+++ hack/e2e.sh
@@ -4,7 +4,9 @@ set -o errexit
 set -o nounset
 set -o pipefail
 
+SCRIPT_ROOT=$(dirname ${BASH_SOURCE})/..
+
 echo -ne "Deploy components for e2e tests"
-./deploy-for-e2e.sh
+${SCRIPT_ROOT}/hack/deploy-for-e2e.sh
 
 echo -ne `(kubectl get pods -o yaml)`
