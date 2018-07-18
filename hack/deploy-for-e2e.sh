1803c0f065e634e115bc5605e97a1df74fc799e0
Krzysztof Kwaśniewski <krzyk@google.com>
2018-07-18 18:06:18 +0200

    remove namespaces

--- hack/deploy-for-e2e.sh
+++ hack/deploy-for-e2e.sh
@@ -6,6 +6,5 @@ set -o pipefail
 
 SCRIPT_ROOT=$(dirname ${BASH_SOURCE})/..
 
-kubectl create namespace default
-kubectl create -f ${SCRIPT_ROOT}/deploy/managedcertificates-crd.yaml --namespace=default
-kubectl create -f ${SCRIPT_ROOT}/deploy/certs-controller.yaml --namespace=default
+kubectl create -f ${SCRIPT_ROOT}/deploy/managedcertificates-crd.yaml
+kubectl create -f ${SCRIPT_ROOT}/deploy/certs-controller.yaml
