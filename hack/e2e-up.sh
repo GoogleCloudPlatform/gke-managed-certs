5e6d457f3169afe3fe4ded8fcc63c0e7e2172f12
Krzysztof Kwaśniewski <krzyk@google.com>
2018-07-19 12:02:55 +0200

    use kubectl 1.11

---
+++ hack/e2e-up.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+set -o errexit
+set -o nounset
+set -o pipefail
+
+SCRIPT_ROOT=$(dirname ${BASH_SOURCE})/..
+
+kubectl create -f ${SCRIPT_ROOT}/deploy/managedcertificates-crd.yaml
+kubectl create -f ${SCRIPT_ROOT}/deploy/certs-controller.yaml
--- hack/e2e.sh
+++ hack/e2e.sh
@@ -5,18 +5,29 @@ set -o nounset
 set -o pipefail
 
 SCRIPT_ROOT=$(dirname ${BASH_SOURCE})/..
+echo -ne "cd to hack\n"
+cd ${SCRIPT_ROOT}/hack
+echo -ne "pwd: `pwd`\n"
 
-echo -ne "Configure registry authentication"
+echo -ne "Configure registry authentication\n"
 gcloud auth activate-service-account --key-file=/etc/service-account/service-account.json
 gcloud auth configure-docker
 
+echo -ne "get kubectl 1.11\n"
+curl -LO https://storage.googleapis.com/kubernetes-release/release/v1.11.0/bin/linux/amd64/kubectl
+chmod +x kubectl
+echo -ne "kubectl version: `./kubectl version`\n"
+
+echo -ne "set namespace default\n"
+kubectl config set-context $(kubectl config current-context) --namespace=default
+
 echo -ne "Delete components created for e2e tests\n"
-${SCRIPT_ROOT}/hack/delete-for-e2e.sh
+./e2e-down.sh
 
 echo -ne "Deploy components for e2e tests\n"
-${SCRIPT_ROOT}/hack/deploy-for-e2e.sh
+./e2e-up.sh
 
 echo -ne `(kubectl get pods -o yaml)`
 
 echo -ne "Delete components created for e2e tests\n"
-${SCRIPT_ROOT}/hack/delete-for-e2e.sh
+./e2e-down.sh
