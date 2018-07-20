1a0036db98b8066c9e7f68a576aa9f5bd1dbd419
Krzysztof Kwaśniewski <krzyk@google.com>
2018-07-20 15:45:54 +0200

    deploy ingress; at start of the test remove all existing ssl certificate objects

--- hack/e2e-down.sh
+++ hack/e2e-down.sh
@@ -6,5 +6,16 @@ set -o pipefail
 
 SCRIPT_ROOT=$(dirname ${BASH_SOURCE})/..
 
+echo -ne "### Remove all existing SslCertificate objects\n"
+for uri in `gcloud alpha compute ssl-certificates list --uri`; do
+  echo -ne "Y\n" || gcloud alpha compute ssl-certificates delete $uri
+done
+
+echo -ne "### Delete ManagedCertificatesController\n"
 kubectl delete -f ${SCRIPT_ROOT}/deploy/certs-controller.yaml --ignore-not-found=true
+
+echo -ne "### Delete CRD\n"
 kubectl delete -f ${SCRIPT_ROOT}/deploy/managedcertificates-crd.yaml --ignore-not-found=true
+
+echo -ne "### Delete ingress\n"
+kubectl delete -f ${SCRIPT_ROOT}/deploy/ingress.yaml --ignore-not-found=true
--- hack/e2e-up.sh
+++ hack/e2e-up.sh
@@ -6,7 +6,14 @@ set -o pipefail
 
 SCRIPT_ROOT=$(dirname ${BASH_SOURCE})/..
 
+echo -ne "### Deploy CRD\n"
 kubectl create -f ${SCRIPT_ROOT}/deploy/managedcertificates-crd.yaml
+
+echo -ne "### Deploy ManagedCertificatesController\n"
 kubectl create -f ${SCRIPT_ROOT}/deploy/certs-controller.yaml
 
+echo -ne "### Deploy test-certificate ManagedCertificate custom object\n"
 kubectl create -f ${SCRIPT_ROOT}/deploy/test-certificate.yaml
+
+echo -ne "### Deploy ingress\n"
+kubectl create -f ${SCRIPT_ROOT}/deploy/ingress.yaml
