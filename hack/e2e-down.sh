9f9b8522e9c4af05f32d6489e420c73d4201c05f
Krzysztof Kwaśniewski <krzyk@google.com>
2018-07-20 15:05:01 +0200

    change order of mc deletion

--- hack/e2e-down.sh
+++ hack/e2e-down.sh
@@ -6,6 +6,7 @@ set -o pipefail
 
 SCRIPT_ROOT=$(dirname ${BASH_SOURCE})/..
 
-kubectl delete -f ${SCRIPT_ROOT}/deploy/managedcertificates-crd.yaml --ignore-not-found=true
-kubectl delete -f ${SCRIPT_ROOT}/deploy/certs-controller.yaml --ignore-not-found=true
 kubectl delete -f ${SCRIPT_ROOT}/deploy/test-certificate.yaml --ignore-not-found=true
+kubectl delete -f ${SCRIPT_ROOT}/deploy/managedcertificates-crd.yaml --ignore-not-found=true
+
+kubectl delete -f ${SCRIPT_ROOT}/deploy/certs-controller.yaml --ignore-not-found=true
--- hack/e2e-up.sh
+++ hack/e2e-up.sh
@@ -8,4 +8,5 @@ SCRIPT_ROOT=$(dirname ${BASH_SOURCE})/..
 
 kubectl create -f ${SCRIPT_ROOT}/deploy/managedcertificates-crd.yaml
 kubectl create -f ${SCRIPT_ROOT}/deploy/certs-controller.yaml
+
 kubectl create -f ${SCRIPT_ROOT}/deploy/test-certificate.yaml
