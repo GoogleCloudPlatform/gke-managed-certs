a6b4e9e34871a667c39cf5a52e66603501d6a6ee
Krzysztof Kwaśniewski <krzyk@google.com>
2018-07-23 18:07:30 +0200

    test-certificate to test1-certificate

--- hack/e2e-up.sh
+++ hack/e2e-up.sh
@@ -12,8 +12,8 @@ kubectl create -f ${SCRIPT_ROOT}/deploy/managedcertificates-crd.yaml
 echo -ne "### Deploy ManagedCertificatesController\n"
 kubectl create -f ${SCRIPT_ROOT}/deploy/certs-controller.yaml
 
-echo -ne "### Deploy test-certificate ManagedCertificate custom object\n"
-kubectl create -f ${SCRIPT_ROOT}/deploy/test-certificate.yaml
+echo -ne "### Deploy test1-certificate ManagedCertificate custom object\n"
+kubectl create -f ${SCRIPT_ROOT}/deploy/test1-certificate.yaml
 
 echo -ne "### Deploy ingress\n"
 kubectl create -f ${SCRIPT_ROOT}/deploy/ingress.yaml
