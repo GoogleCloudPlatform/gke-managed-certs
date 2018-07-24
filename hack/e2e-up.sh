650ddc86e109407609538b05e6b2e02302680300
Krzysztof Kwaśniewski <krzyk@google.com>
2018-07-24 16:09:08 +0200

    add second managed certificate custom object

--- hack/e2e-up.sh
+++ hack/e2e-up.sh
@@ -12,8 +12,9 @@ kubectl create -f ${SCRIPT_ROOT}/deploy/managedcertificates-crd.yaml
 echo -ne "### Deploy ManagedCertificatesController\n"
 kubectl create -f ${SCRIPT_ROOT}/deploy/certs-controller.yaml
 
-echo -ne "### Deploy test1-certificate ManagedCertificate custom object\n"
+echo -ne "### Deploy test1-certificate and test2-certificate ManagedCertificate custom objects\n"
 kubectl create -f ${SCRIPT_ROOT}/deploy/test1-certificate.yaml
+kubectl create -f ${SCRIPT_ROOT}/deploy/test2-certificate.yaml
 
 echo -ne "### Deploy ingress\n"
 kubectl create -f ${SCRIPT_ROOT}/deploy/ingress.yaml
