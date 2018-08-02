1b5b7484299cb9ce8c13abd7c9b3f2bcda402a59
Krzysztof Kwaśniewski <krzyk@google.com>
2018-08-02 11:12:29 +0200

    implement equality for ssl certificate and managed certificate

---
+++ pkg/utils/equals_test.go
@@ -0,0 +1,66 @@
+/*
+Copyright 2018 Google LLC
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    https://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package utils
+
+import (
+	compute "google.golang.org/api/compute/v0.alpha"
+	api "managed-certs-gke/pkg/apis/cloud.google.com/v1alpha1"
+	"testing"
+)
+
+func newMcert(domains []string) *api.ManagedCertificate {
+	return &api.ManagedCertificate {
+		Spec: api.ManagedCertificateSpec {
+			Domains: domains,
+		},
+	}
+}
+
+func newSslCert(domains []string) *compute.SslCertificate {
+	return &compute.SslCertificate {
+		Managed: &compute.SslCertificateManagedSslCertificate {
+			Domains: domains,
+		},
+	}
+}
+
+func TestEquals_emptyObjects(t *testing.T) {
+	mcert := newMcert([]string{})
+	sslCert := newSslCert([]string{})
+
+	if !Equals(mcert, sslCert) {
+		t.Errorf("Empty objects should be equal")
+	}
+}
+
+func TestEquals_differentLengths(t *testing.T) {
+	mcert := newMcert([]string{"abc"})
+	sslCert := newSslCert([]string{})
+
+	if Equals(mcert, sslCert) {
+		t.Errorf("Objects of different length should not be equal")
+	}
+}
+
+func TestEquals_differentOrder(t *testing.T) {
+	mcert := newMcert([]string{"abc", "def"})
+	sslCert := newSslCert([]string{"def", "abc"})
+
+	if !Equals(mcert, sslCert) {
+		t.Errorf("Objects of different order should be equal")
+	}
+}
